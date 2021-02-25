d5f6809f4e7e7dedf1300b318ee761cd0c9fb2c7
Roberto <chova32@gmail.com>
2021-02-24 19:21:59 -0800

    commit od

--- codewars02232021.js
+++ codewars02232021.js
@@ -136,7 +136,71 @@ function solution(M1, M2, m1, m2, V, T) {
   M1 = (m1 * 0.001) / M1;
   M2 = (m2 * 0.001) / M2;
   T = T + 273.15;
-  var R = 0.082;
+  let R = 0.082;
 
   return (((M1 + M2) * R * T) / V) * 1000;
+}
+
+// # Convert boolean values to strings 'Yes' or 'No'.
+// COmplete the method that takes a boolean value and return a "yes" string for true or a "no" string for false.
+
+function boolToWord(bool) {
+  if (bool !== true) {
+    return "No";
+  } else {
+    return "Yes";
+  }
+}
+
+// # Chuck Norris I - Push Ups
+// Chuck Norris loves push ups. That's just a fact. It has been said that when Chuck Norris does a push up, he isn't pushing himself up, he's pushing the world down!
+// Today, Chuck got home from work 10 minutes earlier than his wife. Naturally he decided to bang out some push ups. By the time she arrives home he can have smashed out at least 1000 and barely broken a sweat. He counts them in binary, because he thinks coding is cool, and because he is a massive Badass.
+// When his wife arrives home, she starts talking to Chuck, spoiling his count! Your job is to write a function to isolate Chuck's count, and then work out how many push ups he has done! Return your answer as a number.
+// Careful Chuck doesn't lose count! Even if he does, always return the highest number he counted to! If Chuck's wife has left a list of jobs for him he won't be able to do any push ups... if there is no sign of a count, simply return "CHUCK SMASH!!"
+// In the event someone dares to provide Chuck with something other than a string, return 'FAIL!!'
+// Your code should still pass in the case that the binary is mixed up with other characters - maybe Chuck has a cough... e.g.: '1ee1gf00t10h1011tr00' --> '110010101100' --> 3244.
+// Feel the burn!!
+
+function chuckPushUps(string) {
+  if (typeof string !== "string" || string.length === 0) return "FAIL!!";
+  if (/[01]/.test(string))
+    return Math.max(
+      ...string.split` `
+        .map((v) => v.replace(/[^01]/g, ""))
+        .map((v) => parseInt(v, 2))
+        .filter((v) => !isNaN(v))
+    );
+  return "CHUCK SMASH!!";
+}
+
+// # Reversed strings
+// COmplete the solution so that it reverses the string passed into it
+
+function solution(str) {
+  str = str.split("");
+  str = str.reverse();
+  str = str.join("");
+  return str;
+}
+
+// # Are arrow functions odd?
+// Return the odds from a list
+
+function odds(values) {
+  return values.filter((num) => num % 2 !== 0);
+}
+
+// # Parse nice int from char problem
+// Ask a small girl - "How old are you?" She always says strange things... Let's help her!
+// For correct answer program should return int from 0 to 9.
+// Assume test input string always valid and may look like '1 year old' or '5 years old', etc... The first char is number only.
+
+// function getAge(inputString) {
+//   inputString = inputString.split("");
+//   let age = inputString.find((n) => n === Number);
+//   console.log(age);
+// }
+
+function getAge(inputString) {
+  return parseInt(inputString);
 }
