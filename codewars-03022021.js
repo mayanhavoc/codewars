9bc2ffde6bc926bc0e1ac24a710a9fda96575db5
RobertoM <macadmin@c02rk1vafvh6.attlocal.net>
2021-03-03 15:41:39 -0800

    test commit

--- codewars-03022021.js
+++ codewars-03022021.js
@@ -53,3 +53,21 @@ function isPrime(n) {
   }
   return n !== 1;
 }
+
+// ----
+
+// # +1 Array
+
+// Given an array of integers of any length, return an array that has 1 added to the value represented by the array.
+// the array can't be empty
+// only non-negative, single digit integers are allowed
+// Return nil (or your language's equivalent) for invalid inputs.
+// Examples
+// For example the array [2, 3, 9] equals 239, adding one would return the array [2, 4, 0].
+
+// [4, 3, 2, 5] would return [4, 3, 2, 6]
+
+function upArray(arr) {
+  arr.reduce((acc, cv) => acc + cv + 1);
+  console.log(arr);
+}
